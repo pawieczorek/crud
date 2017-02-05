cbf044ad425777089564b6b6e22abf7984562239
pawieczorek <piotrwieczorek@wp.eu>
2017-02-06 00:43:00 +0100

    Update getProducts.php

--- DatabaseFiles/getProducts.php
+++ DatabaseFiles/getProducts.php
@@ -1,7 +1,7 @@
 <?php
-// Including database connections
+
 require_once 'database_connections.php';
-// mysqli query to fetch all data from database
+
 $query = "SELECT * from products";
 $result = mysqli_query($con, $query);
 $arr = array();
@@ -10,7 +10,7 @@ while($row = mysqli_fetch_assoc($result)) {
 $arr[] = $row;
 }
 }
-// Return json array containing data from the databasecon
+
 echo $json_info = json_encode($arr,JSON_NUMERIC_CHECK);
-//echo $arr;
+
 ?>
